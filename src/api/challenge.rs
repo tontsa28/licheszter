d8f96503e3a2bfad17cc251da919cdea0f612955
tontsa28 <xdman7917@gmail.com>
2022-06-10 18:52:15 +0300

    feat: Decline a challenge

--- src/api/challenge.rs
+++ src/api/challenge.rs
@@ -3,7 +3,7 @@ use crate::client::{Licheszter, LicheszterResult};
 
 impl Licheszter {
     /// Create a challenge
-    pub async fn create_challenge(&self, username: &str, form_params: Option<&[(&str, &str)]>) -> LicheszterResult<EntityChallenge> {
+    pub async fn challenge_create(&self, username: &str, form_params: Option<&[(&str, &str)]>) -> LicheszterResult<EntityChallenge> {
         let addr = format!("{}/api/challenge/{}", self.base, username);
         let mut builder = self.client.post(&addr);
         if let Some(params) = form_params {
@@ -16,6 +16,16 @@ impl Licheszter {
     pub async fn challenge_accept(&self, challenge_id: &str) -> LicheszterResult<()> {
         let addr = format!("{}/api/challenge/{}/accept", self.base, challenge_id);
         let builder = self.client.post(&addr);
+        let ok_json = self.to_model_full::<Value>(builder);
+        assert!(from_value::<bool>(ok_json.await?["ok"].take())?);
+        Ok(())
+    }
+
+    /// Decline a challenge
+    pub async fn challenge_decline(&self, challenge_id: &str, reason: Option<&str>) -> LicheszterResult<()> {
+        let addr = format!("{}/api/challenge/{}/decline", self.base, challenge_id);
+        let form = vec![("reason", reason.map_or("generic".to_string(), String::from))];
+        let builder = self.client.post(&addr).form(&form);
         let ok_json = self.to_model_full::<Value>(builder);
         assert!(from_value::<bool>(ok_json.await?["ok"].take())?);
         Ok(())
