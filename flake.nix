209d530bb4eb0d254e3f3b9a2aa6c406b11b8e14
ahgmn <ahgmn@proton.me>
2025-07-11 18:46:33 +0300

    chore(dev): add command ctest that runs all tests with correct flags

--- flake.nix
+++ flake.nix
@@ -26,12 +26,20 @@
             inherit system;
             overlays = [ rust-overlay.overlays.default ];
           };
+
+          testCommand = "cargo test --release --features=serde-strict,all -- --test-threads 1";
+
+          ctestScript = pkgs.writeShellScriptBin "ctest" ''
+            echo "Running tests with base command <${testCommand}>"
+            ${testCommand} "$@"
+          '';
         in
         {
           devShells.default = pkgs.mkShell {
             packages =
               with pkgs;
               [
+                ctestScript
                 (rust-bin.stable.latest.default.override {
                   extensions = [
                     "rust-src"
@@ -49,6 +57,7 @@
             shellHook = ''
               echo "Welcome to the Rust development environment!"
               echo "Rust version: $(rustc --version)"
+              echo "Run tests with command <ctest> that runs <${testCommand}>"
             '';
           };
         }
